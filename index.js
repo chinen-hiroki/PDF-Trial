5a370c1866958139abe655799df902bf12a3cdd5
chinen-hiroki <66230706+chinen-hiroki@users.noreply.github.com>
2021-02-04 11:09:33 +0900

    update:テストファイル作成

---
+++ index.js
@@ -0,0 +1,9 @@
+var fs = require('fs')
+var Pdf = require('html-pdf')
+var html = fs.readFileSync('./test/index.html', 'utf8');
+var options = { format: 'Letter' };
+
+Pdf.create(html, options).toFile('./index.pdf', function(err, res) {
+    if (err) return console.log(err);
+    console.log(res); // { filename: '/app/businesscard.pdf' }
+  });
