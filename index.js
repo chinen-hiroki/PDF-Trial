73eec1492799a5f9dd7cea72ad35da11b332eafd
chinen-hiroki <66230706+chinen-hiroki@users.noreply.github.com>
2021-02-08 15:46:20 +0900

    update:npm install

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var html = fs.readFileSync('./test/index.html', 'utf8');
 var options = { format: 'Letter' };
 
 Pdf.create(html, options).toFile('./index.pdf', function(err, res) {
-    if (err) return console.log(err);
-    console.log(res); // { filename: '/app/businesscard.pdf' }
-  });
+  if (err) return console.log(err);
+  console.log(res); // { filename: '/app/businesscard.pdf' }
+});
