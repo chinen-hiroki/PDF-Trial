7f2e69342be5659408aaf4c4993989322ad49459
chinen-hiroki <66230706+chinen-hiroki@users.noreply.github.com>
2021-02-09 18:16:44 +0900

    update:ページでの出力

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-var fs = require('fs')
-var Pdf = require('html-pdf')
-var html = fs.readFileSync('./test/index.html', 'utf8');
-var options = { format: 'Letter' };
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const Pdf = require('html-pdf')
+const html = fs.readFileSync('./public/index.html', 'utf8');
+const options = { format: 'Letter' };
+const portForDev = 4000;
 
-Pdf.create(html, options).toFile('./index.pdf', function(err, res) {
+app.set('port', process.env.PORT || portForDev);
+
+Pdf.create(html, options).toFile('./public/index.pdf', function(err, res) {
   if (err) return console.log(err);
   console.log(res); // { filename: '/app/businesscard.pdf' }
 });
+
+express.static('public');
+
+app.use((req, res, next) => {
+  res.send('<h1>Completed</h1><p><a href="./public/index.pdf" download="sample.pdf">PDF</a></p>');
+});
+
+app.listen(app.get('port'));
