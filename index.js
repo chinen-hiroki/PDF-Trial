1bd91f72222d47a1d06347f48c3886e63f9bffff
chinen-hiroki <66230706+chinen-hiroki@users.noreply.github.com>
2021-02-12 16:33:17 +0900

    update:修正追加

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
-const express = require('express')
-const app = express()
-const fs = require('fs')
-const Pdf = require('html-pdf')
-var request = require('request')
-const html = fs.readFileSync('./public/index.html', 'utf8')
-const options = { format: 'Letter' }
-const portForDev = 4000
+const express = require('express');
+const app = express();
+const fs = require('fs');
+var request = require('request');
+const Pdf = require('html-pdf');
+const html = fs.readFileSync('./public/index.html', 'utf8');
+const options = { format: 'Letter' };
+const portForDev = 3000;
 
-app.set('port', process.env.PORT || portForDev)
+app.set('port', process.env.PORT || portForDev);
 
 Pdf.create(html, options).toFile('./public/index.pdf', function(err, res) {
-  if (err) return console.log(err)
-  console.log(res)
-})
+  if (err) return console.log(err);
+  console.log(res);
+});
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 // Create PDF and upload it to AWS S3
+var option = {
+  url: 'https://www.hypdf.com/htmltopdf',
+  method: 'POST',
+  headers: {
+    "content-type": 'application/json',
+  },
+  json: true,
+  parameters: {content: 'HTML', user: 'HYPDF_USER', password: 'HYPDF_PASSWORD'},
+  result: 'BINARY_PDF_DATA'
+}
 request.post(
-    'https://www.hypdf.com/htmltopdf',
-    {
-        json: {
-            user: 'HYPDF_USER',
-            password: 'HYPDF_PASSWORD',
-            content: '<html><body><h1>Title</h1></body></html>',
-            margin_left: '0.5in',
-            grayscale: true,
-            key: 'some_file_name.pdf',
-            public: true
-        }
-    },
     function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            console.log('Public URL: ', body.url)
-            console.log('Number of pages: ', response.headers['hypdf-pages'])
+            console.log('Public URL: ', body.url);
+            console.log('Number of pages: ', response.headers['hypdf-pages']);
         }
     }
-)
+);
 
-app.listen(app.get('port'))
+app.listen(app.get('port'));
