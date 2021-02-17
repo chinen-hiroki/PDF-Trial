4cd26be8be92c8ec5d134ad71d60ae07aa610d34
chinen-hiroki <66230706+chinen-hiroki@users.noreply.github.com>
2021-02-17 11:30:22 +0900

    update:fix

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ request.post(
       json: {
           user: '8248aa12-61f1-4a05-9887-e70c7053f710',
           password: 'RGDrn2AOdW9',
-          content: 'https://webforms-uat.herokuapp.com/reserve/input?type=inv_sfc&gift=1',
+          content: process.env.HTML_PASS,
           margin_left: '0.5in',
           key: 'some_file_name.pdf',
           bucket: 'mybucketeer',
+          callback: 'https://pdf-trial.herokuapp.com/',
           public: true,
           test: true,
       }
