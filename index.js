7f50f1c1be307be0f7feec843373a4dcbbf57ffb
chinen-hiroki <66230706+chinen-hiroki@users.noreply.github.com>
2021-02-17 16:39:44 +0900

    update;

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ request.post(
       json: {
           user: '8248aa12-61f1-4a05-9887-e70c7053f710',
           password: 'RGDrn2AOdW9',
-          content: 'https://pdf-trial.herokuapp.com/',
+          content: 'https://www.shinseibank.com',
           margin_left: '0.5in',
           key: 'some_file_name.pdf',
           bucket: 'mybucketeer',
